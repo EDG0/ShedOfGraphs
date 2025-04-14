63ffb9037eb170f1a09c57e427b9d60a1ce56987
EDG0 <ernest.degres@gmail.com>
2025-04-14 12:47:58 +0200

    history.py geschreven en toegevoegd

---
+++ history.py
@@ -0,0 +1,15 @@
+import time
+
+class HistoryLogger:
+    def __init__(self, filepath="history.txt"):
+        self.filepath = filepath
+
+    def log(self, input_count, output_count, filter_used, passed_graphs):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        passed_str = ",".join(passed_graphs)
+        filter_repr = filter_used if isinstance(filter_used, str) else str(filter_used)
+
+        line = f"{timestamp}\t{input_count}\t{output_count}\t{filter_repr}\t{passed_str}\n"
+
+        with open(self.filepath, "a") as f:
+            f.write(line)
